424555cad947ed864e2989a6a2895526fc0981dc
Sergio Jurado <juradotrillosergio1207@gmail.com>
2024-03-27 14:13:06 +0100

    instalación de daisyUI y probar algunos componentes

---
+++ Frontend/src/components/Card.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Card = () => {
+    return (
+        <>
+            <h1>Card 1</h1>
+            <div className="card w-96 bg-base-100 shadow-xl image-full">
+                <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">Shoes!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+
+            <h1>Card2</h1>
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <figure className="px-10 pt-10">
+                    <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+                </figure>
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title">Shoes!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions">
+                        <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Card
